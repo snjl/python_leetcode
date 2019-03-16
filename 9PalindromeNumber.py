3bd19160537b105c549da7939acfa9ce81645f01
snjl <349245467@qq.com>
2019-03-16 23:45:44 +0800

    9

---
+++ 9PalindromeNumber.py
@@ -0,0 +1,28 @@
+class Solution1:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        len_s = len(s)
+        l = int(len_s / 2)
+        for i in range(l):
+            if s[i] == s[len_s - i - 1]:
+                continue
+            else:
+                return False
+        return True
+
+
+class Solution:
+    def isPalindrome(self, x: 'int') -> 'bool':
+        if x < 0:
+            result = False
+        else:
+            strx = str(x)
+            result = strx == strx[::-1]
+        return result
+
+
+if __name__ == '__main__':
+    x = 122211
+
+    s = Solution()
+    s.isPalindrome(x)
