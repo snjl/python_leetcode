d37bfe1138e6b4225b25cc48270d6087a67b9d94
snjl <349245467@qq.com>
2019-03-20 23:50:59 +0800

    1

---
+++ 970.PowerfulIntegers.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/powerful-integers/
+class Solution:
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> list:
+        pass
+
+
+if __name__ == '__main__':
+
+
+
+    pass
