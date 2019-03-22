ff84bffc062b14b8d1ccbf051787a885bd0aaaaa
snjl <349245467@qq.com>
2019-03-22 23:24:16 +0800

    neo4j_test修改

--- neo4j_test.py
+++ neo4j_test.py
@@ -1,11 +1,7 @@
-from py2neo import Node, Relationship, Graph,NodeMatcher
+from py2neo import Node, Relationship, Graph
 
-graph = Graph("http://db-home.neo4j.shusnjl.cn:17474/db/data/", user='neo4j', password='snjl')
+graph = Graph("http://neo4j.shusnjl.cn:7474/db/data/", user='neo4j', password='snjl')
 # data = graph.data('MATCH (n1)-[r]->(n2) RETURN r, n1, n2 LIMIT 25')
-matcher = NodeMatcher(graph)
-matcher.match("Person", name="Keanu Reeves").limit(20).first()
-for m in matcher:
-    print(m)
 
 a = Node('Person', name='Alice', age=12)
 b = Node('Person', name='Bob')
