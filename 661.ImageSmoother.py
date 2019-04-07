02179219964b6b6c8f6cb9d89e3f6fe820e2d309
snjl <349245467@qq.com>
2019-04-07 23:53:59 +0800

    661未做

---
+++ 661.ImageSmoother.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/image-smoother/
+class Solution:
+    def imageSmoother(self, M: list) ->list:
+
+        pass
+
+
+
+
+if __name__ == '__main__':
+    a = [[1,1,1],
+         [1,0,1],
+         [1,1,1]]
+
