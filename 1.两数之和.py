832df46f0da2948ab36dfef9101f2ee8bd3741bf
snjl <349245467@qq.com>
2019-11-09 14:59:42 +0800

    两数之和 补充

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -1,11 +1,18 @@
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum2(self, nums, target):
         d = {}
         for i, num in enumerate(nums):
             if target - num in d:
                 # 如果目标值减去该num的值在d的键值对里，则返回d[num]=i中的i，如果不存在，则使d[num]=i
                 return d[target - num], i
             d[num] = i
+
+    def twoSum(self, nums, target):
+        d = {}
+        for i in range(len(nums)):
+            if d.get(nums[i]) is not None:
+                return [d.get(nums[i]), i]
+            d[target - nums[i]] = i
 
 
 if __name__ == '__main__':
