70314757c3674014afa46ebc3e51d2703cee0f22
snjl <349245467@qq.com>
2020-02-21 11:54:49 +0800

    增加

--- 81. 搜索旋转排序数组 II.py
+++ 81. 搜索旋转排序数组 II.py
@@ -1,32 +1,35 @@
 class Solution:
-    def search(self, nums, target: int) -> int:
+    def search(self, nums, target: int) -> bool:
         left = 0
         right = len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
+            # print(left,mid,right)
             if nums[mid] == target:
-                return mid
+                return True
             # 左半段是有序的
-            if nums[left] <= nums[mid]:
+            if nums[left] < nums[mid]:
                 # target在这段里
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             # 右半段是有序的
-            else:
+            elif nums[left] > nums[mid]:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
+            else:
+                left += 1
 
-        return -1
+        return False
 
 
 if __name__ == '__main__':
     x = Solution()
-    nums = [5,1,2,3,4]
+    nums = [1,3,1,1,1]
 
-    target = 1
+    target = 3
     print(x.search(nums, target))
