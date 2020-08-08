d345791173558deb28435e68a59e4972f3df1401
snjl <349245467@qq.com>
2020-08-09 00:07:04 +0800

    增加

--- 55. 跳跃游戏.py
+++ 55. 跳跃游戏.py
@@ -1,13 +1,17 @@
 class Solution:
     def canJump(self, nums) -> bool:
+        jump_list = [False for _ in nums]
+        jump_list[0] = True
+        for i in range(len(nums)):
+            for j in range(0, i):
+                if jump_list[i] is True and i + nums[j] >= j:
+                    jump_list[j] = True
+                    break
 
-
-
-
-        pass
+        return jump_list[-1]
 
 
 if __name__ == '__main__':
     x = Solution()
 
-    print(x.canJump([2,3,1,1,4]))
+    print(x.canJump([3, 2, 1, 0, 4]))
