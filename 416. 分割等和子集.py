ca1e0aeb0cd4aec717c0a834826713c626f7daad
snjl <349245467@qq.com>
2020-08-12 00:03:42 +0800

    增加

---
+++ 416. 分割等和子集.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPartition(self, nums) -> bool:
+
+
+        
+
+
+
+
+        pass
+
+
+
+if __name__ == '__main__':
+    x = Solution()
+    print(x.canPartition([1, 5, 11, 5]))
---
+++ 647. 回文子串.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+
+        
+
+
+
+
+        pass
+
+
+if __name__ == '__main__':
+    x = Solution()
+
+    print(x.countSubstrings('aaa'))
